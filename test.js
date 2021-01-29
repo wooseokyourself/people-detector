4470eb4ced7a386b57b3cd1a0e59ab3c771a98a4
Wooseok Choi <21500740@handong.edu>
2021-01-29 14:12:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 var addon = require('bindings')('people-detector');
 var obj = new addon.Yolo_cpu();
-console.log( obj.start('image.jpeg', 416) ); // people number 
+// <arg1> = Input image file path with extension
+// <arg2> = Output image file path with extension
+// <arg3> = Size of first network of yolov3. The larger, the slower but the smaller objects are better detected.
+// recommended: 320, 416, 608, 800 or other multiples of 32.
+console.log( obj.start('image.jpeg', 'image-result.jpge', 416) );
